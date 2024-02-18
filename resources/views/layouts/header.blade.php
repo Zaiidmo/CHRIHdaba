0386e473f4341f450c42ed2dff703f0ac0bcf49e
Zaiidmo <zaiidmoumnii@gmail.com>
2024-02-18 17:59:19 +0100

    FrontOffice : Editing Links in Home page

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -92,7 +92,7 @@
                     <div>
                         <span class="hidden" id="util_data"></span>
                         <a class=" py-1.5 px-3 m-1 text-center bg-gray-100  rounded-md text-black hover:bg-gray-100 lg:inline-block "
-                            href="https://tailwindflex.com/login">
+                            href="/">
                             Sign In
                         </a>
                     </div>
@@ -110,7 +110,7 @@
 
             <div class="flex items-center mb-8">
                 <a class="mr-auto text-2xl font-bold text-black text-violet-600 dark:text-gray-100"
-                    href="https://tailwindflex.com/">
+                    href="/">
                     TailwindFlex
                 </a>
 
@@ -140,31 +140,14 @@
 
             </div>
 
-            <div class="mt-auto">
-
-                <div class="pt-6">
-                    <button id="theme-toggle-2" type="button"
-                        class=" py-2.5 w-[97.6%] mb-3 rounded-xl flex justify-center align-middle py-1.5 px-3 m-1 text-center bg-gray-100 border border-gray-300 rounded-md text-black  hover:bg-gray-100 dark:text-gray-300 dark:bg-gray-700 hover:bg-gray-100 dark:hover:bg-gray-700">
-                        <svg id="theme-toggle-dark-icon-2" class="w-6 h-6 hidden" fill="currentColor"
-                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-                        </svg>
-                        <svg id="theme-toggle-light-icon-2" class="w-6 h-6 hidden" fill="currentColor"
-                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                                fill-rule="evenodd" clip-rule="evenodd"></path>
-                        </svg>
-                    </button>
-
-                    <a class="block py-1.5 px-3 m-1 text-center bg-gray-100 border border-gray-300 rounded-md text-black  hover:bg-gray-100 dark:text-gray-300 dark:bg-gray-700 px-4 py-3 mb-3 text-sm text-center font-semibold rounded-xl"
-                        href="/login">
-                        Sign in
+            <div class="lg:flex">
+                <div>
+                    <span class="hidden" id="util_data"></span>
+                    <a class=" py-1.5 px-3 m-1 text-center bg-gray-100  rounded-md text-black hover:bg-gray-100 lg:inline-block "
+                        href="/">
+                        Sign In
                     </a>
-
-
                 </div>
-
             </div>
         </nav>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,14 +16,14 @@
                     </div>
                     <div>
                         @auth
-                            <a href="discover"
+                            <a href="product.index"
                                 class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-800  rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                                Discover Out Store
+                                Discover Our Store
                             </a>
                         @else
-                            <a href="discover"
+                            <a href="product"
                                 class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-800  rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                                Discover Out Store
+                                Discover Our Store
                             </a>
                             <a href="register" id="register"
                                 class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-gray-800 dark:text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
