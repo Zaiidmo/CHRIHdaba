e2cd1d432037777fb2e696b544e36a1f169e77be
Zaiidmo <zaiidmoumnii@gmail.com>
2024-02-19 12:25:33 +0100

    FrontOffice : Fetching data to the Products Page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        return view('product.index');
+        $products = Product::all();
+        return view('product.index', compact('products'));
     }
 
     public function show()
