d91ad89edbfbf658ffd0a319c1761a7c0b3857cb
BALIBLA MERIEM <balibla.meriem@gmail.com>
2024-02-20 12:09:16 +0100

    Page product: Update

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -127,9 +127,15 @@
                                     class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
                                 <div class="flex items-center justify-between">
                                     <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-                                    <a href="#"
+                                    {{-- <a href="#"
                                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add
-                                        to cart</a>
+                                        to cart</a> --}}
+
+                                    <form action="card">
+                                        <button  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add
+                                            to cart
+                                        </button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
